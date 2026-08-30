404a5e135e3391b907d174f8feece06c420d7615
givtszs <givtszs@gmail.com>
2026-08-30 16:01:09 +0300

    Refactor the single file project structure into regular modules

---
+++ config.js
@@ -0,0 +1,12 @@
+export const TIMELINE_CONFIGS = {
+  "energy-sources": {
+    label: "Energy Sources",
+    workbookUrl: "https://docs.google.com/spreadsheets/d/1FzvsnTiFBMbd4qTl413WTfStqPECJiQYJ9GOkpUM7s0/export?format=xlsx"
+  },
+  "energy-uses": {
+    label: "Energy Uses",
+    workbookUrl: "https://docs.google.com/spreadsheets/d/1ctHZncWlVKlUTrEnv-Px6aP29P1eYUW9OgExkB9V0OY/export?format=xlsx"
+  }
+};
+
+export const ACTIVE_TAB_KEY = "energy-sources";
